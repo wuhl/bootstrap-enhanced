614dbfb68facac994b2784f03bc3198b4dbe9fda
Wolfgang Uhl <wolfgang.uhl@googlemail.com>
2013-10-15 17:57:05 +0200

    doc/pages added

--- bootstrap-enhanced.gemspec
+++ bootstrap-enhanced.gemspec
@@ -18,7 +18,8 @@ Gem::Specification.new do |spec|
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.add_dependency "twitter-bootstrap-rails"
+  spec.add_dependency "twitter-bootstrap-rails", "~> 2.2.8"
+  spec.add_dependency "high_voltage", "~> 2.0.0"
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
 end
--- lib/generators/bootstrapenhanced/install/install_generator.rb
+++ lib/generators/bootstrapenhanced/install/install_generator.rb
@@ -28,19 +28,23 @@ module Bootstrapenhanced
       def insert_menu_bar_and_default_pages
         copy_file "_menu.html.erb", "app/views/layouts/_menu.html.erb"
         empty_directory "app/views/pages"
-        
-        about_file = "pages/about.html.erb"
-        if options[:about_file]
-          about_file = options[:about_file]
+        if Dir.exist? "doc/pages"
+          files = Dir.glob("doc/pages/*") - ['backup']
+          FileUtils.cp_r files, "app/views/pages"
+          puts "      Pages installed"
+        else
+          about_file = "pages/about.html.erb"
+          if options[:about_file]
+            about_file = options[:about_file]
+          end
+          copy_file about_file, "app/views/pages/about.html.erb"
+          
+          contact_file = "pages/contact.html.erb"
+          if options[:contact_file]
+            contact_file = options[:contact_file]
+          end
+          copy_file contact_file, "app/views/pages/contact.html.erb"
         end
-        copy_file about_file, "app/views/pages/about.html.erb"
-        
-        contact_file = "pages/contact.html.erb"
-        if options[:contact_file]
-          contact_file = options[:contact_file]
-        end
-        copy_file contact_file, "app/views/pages/contact.html.erb"
-        
         copy_file "high_voltage.rb", "config/initializers/high_voltage.rb"
       end
 
@@ -64,6 +68,58 @@ module Bootstrapenhanced
         gsub_file "lib/templates/erb/scaffold/index.html.erb", "<table class=\"datatable display\">", "<table class=\"datatable display table table-striped table-bordered\">"
       end
 
+      def add_default_images
+        if Dir.exist? "doc/images"
+          files = Dir.glob("doc/images/*")
+          FileUtils.cp_r files, "app/assets/images"
+          puts "      Images installed"
+        end
+      end
+      
+      def add_default_stylesheets
+        if Dir.exist? "doc/stylesheets"
+          files = Dir.glob("doc/stylesheets/*") - ['backup']
+          FileUtils.cp_r files, "app/assets/stylesheets"
+          puts "      Stylesheets installed"
+        end
+      end
+
+      def add_default_application_helper
+        if File.exist? "doc/helpers/application_helper.rb"
+          if File.exist? "app/helpers/application_helper.rb"
+            FileUtils.remove "app/helpers/application_helper.rb"
+          end
+          files = Dir.glob("doc/helpers/*")
+          FileUtils.cp_r files, "app/helpers"
+          puts "      Helpers installed"
+        end
+      end
+
+      def add_default_layouts
+        if Dir.exist? "doc/layouts"
+          files = Dir.glob("doc/layouts/*")
+          FileUtils.cp_r files, "app/views/layouts"
+          puts "      Layouts installed"
+        end
+      end
+      
+      def add_default_shared
+        if Dir.exist? "doc/shared"
+          empty_directory "app/views/shared"
+          files = Dir.glob("doc/shared/*")
+          FileUtils.cp_r files, "app/views/shared"
+          puts "      Shared installed"
+        end
+      end
+ 
+      def add_default_locales
+        if Dir.exist? "doc/locales"
+          files = Dir.glob("doc/locales/*")
+          FileUtils.cp_r files, "config/locales/de"
+          puts "      Locales installed"
+        end
+      end
+     
     end
   end
 end
